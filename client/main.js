15647cd7aa288bca6d43c6ebdf45def20027df09
Samuel DAUZON <git@dauzon.com>
2017-03-06 00:21:04 +0100

    ajout fonction dans helper. MomentJS

--- client/main.js
+++ client/main.js
@@ -3,20 +3,6 @@ import { Template } from 'meteor/templating';
 Template.accueil.helpers({
   dateAujourdhui: new Date(),
   formatDateTime: function(date) {
-    var date_texte = '';
-    if (date.getDate() < 10) {
-      date_texte += '0';
-    }
-    date_texte += date.getDate()+'/';
-    var numero_mois = date.getMonth() + 1;
-    if (numero_mois < 10) {
-      date_texte += '0';
-    }
-    date_texte += numero_mois+'/';
-    date_texte += date.getFullYear()+' ';
-    date_texte += date.getHours()+':';
-    date_texte += date.getMinutes()+':';
-    date_texte += date.getSeconds();
-    return date_texte;
+    return moment(date).format('DD/MM/YYYY HH:mm:ss');
   }
 });
