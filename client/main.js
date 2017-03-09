480ec2cc5db790cf45139113533e013d33d65eb1
Samuel DAUZON <git@dauzon.com>
2017-03-09 23:43:09 +0100

    Contexte de données

--- client/main.js
+++ client/main.js
@@ -1,16 +1,23 @@
 import { Template } from 'meteor/templating';
 
-Template.listeUtilisateur.helpers({
-  listeUtilisateur: [
-    {
-      prenomNom: 'Jean',
-      age: 28,
-      fonction: 'Lead développeur'
+Template.evolution2Jours.helpers({
+  action: {
+    nom: 'AXA',
+    place: 'Euronext Paris',
+    produitFinancier: 'Action',
+    aujourdhui: {
+      ouverture: 23.52,
+      plusBas: 23.465,
+      plusHaut: 23.935,
+      fermeture: 23.725,
+      volume: 5618000
     },
-    {
-      prenomNom: 'Antoine',
-      age: 25,
-      fonction: 'Développeur'
+    hier: {
+      ouverture: 23.575,
+      plusBas: 23.405,
+      plusHaut: 23.66,
+      fermeture: 23.58,
+      volume: 5216000
     }
-  ]
+  }
 });
