39db587fbb12f611baeccb4848b1c1a401efe24c
Samuel DAUZON <git@dauzon.com>
2017-03-12 23:42:28 +0100

    Simple événement

--- client/main.js
+++ client/main.js
@@ -1,23 +1,13 @@
 import { Template } from 'meteor/templating';
 
-Template.evolution2Jours.helpers({
-  action: {
-    nom: 'AXA',
-    place: 'Euronext Paris',
-    produitFinancier: 'Action',
-    aujourdhui: {
-      ouverture: 23.52,
-      plusBas: 23.465,
-      plusHaut: 23.935,
-      fermeture: 23.725,
-      volume: 5618000
-    },
-    hier: {
-      ouverture: 23.575,
-      plusBas: 23.405,
-      plusHaut: 23.66,
-      fermeture: 23.58,
-      volume: 5216000
-    }
+Template.ajouterMessage.events({
+  'submit form[name="ajouterMessageForm"]': function(event) {
+    event.preventDefault();
+
+    var message = event.target.message.value;
+    alert(message);
+    event.target.message.value = '';
+
+    event.stopPropagation();
   }
 });
