bd567c8b1d6bdc9ad4871185eb535ddd177df45a
Samuel DAUZON <git@dauzon.com>
2017-03-26 23:57:18 +0200

    Titre HTML

--- router/routes.js
+++ router/routes.js
@@ -4,16 +4,26 @@ Router.configure({
 
 Router.route('/', {
   template: 'accueil',
-  name: 'accueil'
+  name: 'accueil',
+  onBeforeAction: function () {
+    document.title = "Accueil Meteor";
+  },
 });
 
 Router.route('/a-propos', {
-  template: 'aPropos'
+  template: 'aPropos',
+  name: 'aPropos',
+  onBeforeAction: function () {
+    document.title = "A propos";
+  },
 });
 
 Router.route('/utilisateur/profil/:pseudo', {
   template: 'profilUtilisateur',
   name: 'utilisateur.profil',
+  onBeforeAction: function () {
+    document.title = "Profil utilisateur : "+this.params.pseudo;
+  },
   data: function() {
     return {
       'utilisateur': {
